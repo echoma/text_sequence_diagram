eebb0f39d77f845bc74721667adb4cc88c23d312
echo <fatmck@163.com>
2016-04-16 03:43:31 +0800

    full support for UML seq-diagram

--- README.md
+++ README.md
@@ -12,29 +12,50 @@ A cross platform tool for drawing UML sequence diagram in simple text.
 
 * Simple pure text syntax to draw beautiful sequence diagram.
 * Cross platform, given by [**nw.js**](http://nwjs.io/) and HTML5.
-* Adjust the diagram view size as you wish.
-* You can integrate the "text to diagram" library to your own application. See [this page](not_implemented) for more detail.
-* Save text into local file(**not implemented yet**)
+* Adjust the diagram view size and zoom ratio as you wish.
+* You can integrate the "text to diagram" library to your own application. See [example_integeration.html](https://github.com/echoma/text_sequence_diagram/blob/master/example_integeration.html) for more detail.
 
 ### Syntax
 
+* Title and Author and so on
+```scala
+Title @ v1.0 @ 20160415 @ Author
+Title @ @ @ Author
 ```
-From -> To : Note
+
+* Message
+```scala
+From Object -> To Object : Message Content
 ```
-or
+
+* Return Message
+```scala
+From Object --> To Object : Message Content
 ```
-From --> To : Note
+
+* Self Message
+```scala
+Object : Message Content
+```
+
+* Bottom Description
+```scala
+[ Bottom Description Line 1 ]
+[ Bottom Description Line 2 ]
+```
+
+* Comment
+```scala
+# Comment Line
+# Another Comment Line
 ```
 
 * Example:
-
-```
-Conn Server -> Safe Domain GW : new order request
+```scala
+Conn Server -> Safe Domain GW : single order request
 Safe Domain GW -> Trade Gateway : authenticated
 Trade Gateway -> Logic Control : internal protocol
 Logic Control --> Trade Gateway : success response
 Trade Gateway --> Safe Domain GW : success response
 Safe Domain GW --> Conn Server : response
 ```
-
-
--- js/seq_diagram.js
+++ js/seq_diagram.js
@@ -2,13 +2,20 @@ function SequenceDiagram(jq_con_dom)
 {
 	var t = this;
 	t.subsys_list = [];
+	t.line_top = null;
 	t.line_list = [];
+	t.line_bottom = null;
 	t.jq_con_dom = jq_con_dom;
 	t.jq_dom = $('<div class="seqdiagram"></div>');
+	t.jq_dom_title = $('<div class="seqdiagram_title"><div class="box west"></div><div class="center"></div><div class="box east"></div></div>');
 	t.jq_dom_sysbox = $('<div class="seqdiagram_sysbox"></div>');
-	t.jq_dom.append(t.jq_dom_sysbox);
+	t.jq_dom_bottom = $('<div class="seqdiagram_bottom"><div class="side"></div><div class="center"></div><div class="side"></div></div>');
 	t.jq_con_dom.append(t.jq_dom);
 	t.dock_color_list = ['SteelBlue','coral','DeepSkyBlue','lightgreen','SlateBlue','HotPink','Olive','Sienna','Tan'];
+	t.regex_msg = /^(.+?)(\-{1,2}\>)([^:]*)(:(.*))+$/;
+	t.regex_self_msg = /^([^\#>]+?)(:(.*))+$/;
+	t.regex_title = /^([^@]+)?@([^@]*)?@([^@]*)?@([^@]+)$/;
+	t.regex_bottom_description = /^\s*?\[([^\]]*)\]\s*$/;
 	t.px2em=function(n) { return n*0.0625; };
 	t.interval=setInterval(function(){ t.recal_line(); },1000);
 	t.ensure_subsys=function(name)
@@ -22,7 +29,7 @@ function SequenceDiagram(jq_con_dom)
 		}
 		var idx  = t.subsys_list.length;
 		t.subsys_list.push({'idx':idx,'name':name,'linedot':[]});
-		console.debug('add subsys '+name+' idx '+idx);
+		//console.debug('add subsys '+name+' idx '+idx);
 		return idx;
 	};
 	t.gen_line_seg=function(sys)
@@ -74,23 +81,51 @@ function SequenceDiagram(jq_con_dom)
 	};
 	t.draw=function(o)
 	{
-		//save line list
+		//save data
+		var data = o;
 		if (typeof(o)=='string')
-			t.line_list = t.parse(o);
-		else
-			t.line_list = o;
+		{
+			data = t.parse(o);
+		}
+		//console.log(data);
+		t.subsys_list = [];
+		t.line_top = data['top'];
+		t.line_bottom = data['bottom'];
+		t.line_list = data['line_list'];
 		//clear dom before draw
+		t.jq_dom.children().remove();
 		t.jq_dom_sysbox.children().remove();
-		t.jq_dom.children('.arrow_line').remove();
+		//t.jq_dom.children('.arrow_line').remove();
+		//t.jq_dom.children('.self_msg').remove();
+		//------------draw title
+		if (null!=t.line_top)
+		{
+			//console.log("draw top");
+			t.jq_dom.append(t.jq_dom_title);
+			t.jq_dom_title.find('.west').text(t.line_top['title'] + ' ' + t.line_top['ver']);
+			t.jq_dom_title.find('.east').text(t.line_top['author'] + ' ' + t.line_top['date']);
+		}
+		//------------draw subsys box
+		t.jq_dom.append(t.jq_dom_sysbox);
 		//cal subsys list
 		for (var i=0; i<t.line_list.length; ++i)
 		{
-			var f_idx = t.ensure_subsys( t.line_list[i]['from'] );
-			t.subsys_list[f_idx]['linedot'].push(i);
-			t.line_list[i]['from_sys_idx'] = f_idx;
-			var t_idx = t.ensure_subsys( t.line_list[i]['to'] );
-			t.subsys_list[t_idx]['linedot'].push(i);
-			t.line_list[i]['to_sys_idx'] = t_idx;
+			var line = t.line_list[i];
+			if (line['type']=='msg')
+			{
+				var f_idx = t.ensure_subsys( line['from'] );
+				t.subsys_list[f_idx]['linedot'].push(i);
+				line['from_sys_idx'] = f_idx;
+				var t_idx = t.ensure_subsys( line['to'] );
+				t.subsys_list[t_idx]['linedot'].push(i);
+				line['to_sys_idx'] = t_idx;
+			}
+			else if (line['type']=='self_msg')
+			{
+				var idx = t.ensure_subsys( line['name'] );
+				t.subsys_list[idx]['linedot'].push(i);
+				line['sys_idx'] = idx;
+			}
 		}
 		//generate line seg according to the linedot list
 		for (var i=0; i<t.subsys_list.length; ++i)
@@ -127,23 +162,35 @@ function SequenceDiagram(jq_con_dom)
 		for (var i=0; i<t.line_list.length; ++i)
 		{
 			var line = t.line_list[i];
-			var lfi = line['from_sys_idx'];
-			var lti = line['to_sys_idx'];
-			var line_class = parseInt(line['is_dash'])?'dashline':'line';
 			var dom_line = null;
-			if (lfi<lti)
+			if (line['type']=='msg')
 			{
-				dom_line = $('<div class="arrow_line">\
-					<div class="'+line_class+'"></div>\
-					<div class="arrow_to_right"></div>\
-					<div class="note"></div>\
-				</div>');
+				var lfi = line['from_sys_idx'];
+				var lti = line['to_sys_idx'];
+				var line_class = parseInt(line['is_dash'])?'dashline':'line';
+				if (lfi<lti)
+				{
+					dom_line = $('<div class="arrow_line">\
+						<div class="'+line_class+'"></div>\
+						<div class="arrow_to_right"></div>\
+						<div class="note"></div>\
+					</div>');
+				}
+				else
+				{
+					dom_line = $('<div class="arrow_line">\
+						<div class="arrow_to_left"></div>\
+						<div class="'+line_class+'"></div>\
+						<div class="note"></div>\
+					</div>');
+				}
 			}
-			else
+			else if (line['type']=='self_msg')
 			{
-				dom_line = $('<div class="arrow_line">\
-					<div class="arrow_to_left"></div>\
-					<div class="'+line_class+'"></div>\
+				var li = line['sys_idx'];
+				dom_line = $('<div class="self_msg_line">\
+					<div class="link"></div>\
+					<div class="arrow"></div>\
 					<div class="note"></div>\
 				</div>');
 			}
@@ -153,6 +200,21 @@ function SequenceDiagram(jq_con_dom)
 		}
 		$(window).resize( function(){ t.recal_line(); } );
 		setTimeout(function(){t.recal_line();},1);
+		//------------draw bottom
+		if (null!=t.line_bottom)
+		{
+			//console.log("draw bottom");
+			t.jq_dom.append(t.jq_dom_bottom);
+			var tmp = $('<div></div>');
+			var html = '';
+			for (var i=0; i<t.line_bottom.length; ++i)
+			{
+				if (i!=0)
+					html += '<br/>';
+				html += tmp.text(t.line_bottom[i]['text']).html();
+			}
+			t.jq_dom_bottom.find('.center').html(html);
+		}
 	};
 	t.recal_line=function()
 	{
@@ -161,65 +223,134 @@ function SequenceDiagram(jq_con_dom)
 		{
 			var line = t.line_list[i];
 			var dom_line = t.jq_dom.children('[name=line'+i+']');
-			var dom_from_subsys = t.jq_dom_sysbox.children('[name=subsys'+line['from_sys_idx']+']');
-			var dom_to_subsys = t.jq_dom_sysbox.children('[name=subsys'+line['to_sys_idx']+']');
-			var dom_left = null, dom_right = null;
-			if (line['from_sys_idx'] < line['to_sys_idx'])//line is left to right
+			if (line['type']=='msg')
 			{
-				dom_left = dom_from_subsys;
-				dom_right = dom_to_subsys;
+				var dom_from_subsys = t.jq_dom_sysbox.children('[name=subsys'+line['from_sys_idx']+']');
+				var dom_to_subsys = t.jq_dom_sysbox.children('[name=subsys'+line['to_sys_idx']+']');
+				var dom_left = null, dom_right = null;
+				if (line['from_sys_idx'] < line['to_sys_idx'])//line is left to right
+				{
+					dom_left = dom_from_subsys;
+					dom_right = dom_to_subsys;
+				}
+				else//line is right to left
+				{
+					dom_left = dom_to_subsys;
+					dom_right = dom_from_subsys;
+				}
+				//calculate line's left and right end position, calculate width
+				var left_x = dom_left.offset().left + dom_left.width()/2 + dock_width/2;
+				var right_x = dom_right.offset().left + dom_right.width()/2 - dock_width/2;
+				dom_line.css('width',right_x-left_x);
+				dom_line.css('left', left_x);
+				//calculate top
+				var vseg_cnt = t.line_list.length+1;
+				var dom_bodybox = dom_from_subsys.children('.bodybox');
+				var avg_seg_height = dom_bodybox.height()/vseg_cnt;
+				var bodybox_top = dom_bodybox.offset().top;
+				//console.log(bodybox_top+'+('+(i+1)+'*'+avg_seg_height+')');
+				dom_line.css('top',bodybox_top+(i+1)*avg_seg_height-5);
 			}
-			else//line is right to left
+			else if (line['type']=='self_msg')
 			{
-				dom_left = dom_to_subsys;
-				dom_right = dom_from_subsys;
+				var dom_subsys = t.jq_dom_sysbox.children('[name=subsys'+line['sys_idx']+']');
+				var left_x = dom_subsys.offset().left + dom_subsys.width()/2 + dock_width/2;
+				dom_line.css('left', left_x).css('width', dom_subsys.width());
+				//calculate top
+				var vseg_cnt = t.line_list.length+1;
+				var dom_bodybox = dom_subsys.children('.bodybox');
+				var avg_seg_height = dom_bodybox.height()/vseg_cnt;
+				var bodybox_top = dom_bodybox.offset().top;
+				dom_line.css('top',bodybox_top+(i+1)*avg_seg_height-5);
 			}
-			//calculate line's left and right end position, calculate width
-			var left_x = dom_left.offset().left + dom_left.width()/2 + dock_width/2;
-			var right_x = dom_right.offset().left + dom_right.width()/2 - dock_width/2;
-			dom_line.css('width',right_x-left_x);
-			dom_line.css('left', left_x);
-			//calculate top
-			var vseg_cnt = t.line_list.length+1;
-			var dom_bodybox = dom_from_subsys.children('.bodybox');
-			var avg_seg_height = dom_bodybox.height()/vseg_cnt;
-			var bodybox_top = dom_bodybox.offset().top;
-			//console.log(bodybox_top+'+('+(i+1)+'*'+avg_seg_height+')');
-			dom_line.css('top',bodybox_top+(i+1)*avg_seg_height-5);
 		}
 	};
-	t.parse=function(t)
+	t.parse=function(txt)
 	{
-		var re = /^(.*?)(\-{1,2}\>)([^:]*):?(.*)$/;
-		var lines = t.split("\n");
-		var line_list = [];
+		var lines = txt.split("\n");
+		var data = {'top':null, 'line_list':[], 'bottom':null};
 		for (var i=0; i<lines.length; ++i)
 		{
-			var mats = lines[i].match(re);
-			if (null==mats)
+			var obj = t.parse_as_msg(lines[i]);
+			if (null!=obj)
+			{
+				data['line_list'].push(obj);
 				continue;
+			}
+			obj = t.parse_as_self_msg(lines[i]);
+			if (null!=obj)
+			{
+				data['line_list'].push(obj);
+				continue;
+			}
+			obj = t.parse_as_title(lines[i]);
+			if (null!=obj)
+			{
+				data['top'] = obj;
+				continue;
+			}
+			obj = t.parse_as_bottom_description(lines[i]);
+			if (null!=obj)
+			{
+				if (null==data['bottom'])
+					data['bottom'] = [];
+				data['bottom'].push(obj);
+				continue;
+			}
+		}
+		return data;
+	};
+	t.parse_as_msg=function(line)
+	{
+		var mats = line.match(t.regex_msg);
+		if (null!=mats)
+		{
 			var name_begin = mats[1].trim();
 			var op_str = mats[2].trim();
 			var name_end = mats[3].trim();
-			var note = mats[4].trim();
-			var obj = {'from':name_begin, 'to':name_end};
+			var note = mats[5].trim();
+			var obj = {'type':'msg', 'from':name_begin, 'to':name_end, 'note':note};
 			if (op_str=='->')
 			{
-				obj['type'] = 'line';
-				obj['note'] = note;
+				obj['is_dash'] = 0;
 			}
 			else if (op_str=='-->')
 			{
-				obj['type'] = 'line';
 				obj['is_dash'] = 1;
-				obj['note'] = note;
 			}
 			else
 			{
-				continue;
+				return null;
 			}
-			line_list.push(obj);
+			return obj;
 		}
-		return line_list;
+		return null;
+	};
+	t.parse_as_self_msg=function(line)
+	{
+		var mats = line.match(t.regex_self_msg);
+		if (null!=mats)
+		{
+			return {'type':'self_msg', 'name':mats[1].trim(), 'note':mats[3].trim()};
+		}
+		return null;
+	};
+	t.parse_as_title=function(line)
+	{
+		var mats = line.match(t.regex_title);
+		if (null!=mats)
+		{
+			return {'type':'title', 'title':mats[1].trim(), 'ver':mats[2].trim(), 'date':mats[3].trim(), 'author':mats[4].trim()};
+		}
+		return null;
+	};
+	t.parse_as_bottom_description=function(line)
+	{
+		var mats = line.match(t.regex_bottom_description);
+		if (null!=mats)
+		{
+			return {'type':'bottom_description', 'text':mats[1].trim()};
+		}
+		return null;
 	};
 }
